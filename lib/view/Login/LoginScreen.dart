6369fc34c999e670cd4fde06b3aafd8574559313
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-06-25 16:07:34 +0500

    1.Making Login Ui

--- lib/view/Login/LoginScreen.dart
+++ lib/view/Login/LoginScreen.dart
@@ -1,4 +1,5 @@
 import 'package:blog_app/Resources/Color/colors.dart';
+import 'package:blog_app/Resources/Components/RoundButton/RoundButton.dart';
 import 'package:blog_app/Resources/Components/TextInputField/InputField.dart';
 import 'package:blog_app/Utils/Utils.dart';
 import 'package:blog_app/view_model/Controller/Login/LoginController.dart';
@@ -20,6 +21,17 @@ class _LoginScreenState extends State<LoginScreen> {
      final controller = Get.put(LoginController());
 
   final _formkey = GlobalKey<FormState>();
+  
+  
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    controller.passwordFocus.value.dispose();
+    controller.passwordController.value.dispose();
+    controller.emailFocus.value.dispose();
+    controller.emailController.value.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,34 +50,47 @@ class _LoginScreenState extends State<LoginScreen> {
            backgroundColor: AppColor.pinkColor,
          ),
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Form(
-                  child: Column(
-                    children: [
-                      InputField(myController:controller.emailController.value, focusNode: controller.emailFocus.value, onFieldSubmitted: (value){
+          child:Obx((){
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Form(
+                    child: Column(
+                      children: [
+                        InputField(myController:controller.emailController.value, focusNode: controller.emailFocus.value, onFieldSubmitted: (value){
 
-                      }, obsecureText: false, hint:'Em_ail'.tr , keyboardType: TextInputType.text, onValidator: (value){
-                        if(value!.isEmpty){
-                          Utils.snackBar('Em_ail'.tr, '_email'.tr);
-                        }
-                      }, icon:Icons.email),
-                      SizedBox(height: height * .02,),
-                      InputField(myController: controller.passwordController.value, focusNode: controller.passwordFocus.value, onFieldSubmitted: (value){}, obsecureText: true, hint: 'pass_word'.tr, keyboardType: TextInputType.number, onValidator: (value){
-                        if(value!.isEmpty){
-                          Utils.snackBar('pass_word'.tr, '_email'.tr);
-                        }
-                      }, icon: Icons.lock)
+                        }, obsecureText: false, hint:'Em_ail'.tr , keyboardType: TextInputType.text, onValidator: (value){
+                          if(value!.isEmpty){
+                            Utils.snackBar('Em_ail'.tr, '_email'.tr);
+                          }
+                        }, icon:Icons.email),
+                        SizedBox(height: height * .02,),
+                        InputField(myController: controller.passwordController.value, focusNode: controller.passwordFocus.value, onFieldSubmitted: (value){}, obsecureText: true, hint: 'pass_word'.tr, keyboardType: TextInputType.number, onValidator: (value){
+                          if(value!.isEmpty){
+                            Utils.snackBar('pass_word'.tr, '_email'.tr);
+                          }
+                        }, icon: Icons.lock)
 
-                    ],
-                  )
+                      ],
+                    )
 
-              )
+                ),
+                SizedBox(height: height * 0.2,),
+                RoundButton(title: 'Login', onPress: (){
+                    if(_formkey.currentState!.validate()){
+                      String email = controller.emailController.value.text;
+                      String password = controller.passwordController.value.text;
+                      controller.loginFtn(email, password, context);
+                    }
+                })
 
-            ],
-          ),
+              ],
+            );
+          }),
+          
+          
+          
         ),
     );
   }
